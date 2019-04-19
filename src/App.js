b93e89cf199eec7add0137289efec79916a6678d
Truong <truong.nguyen@persol.co.jp>
2019-04-19 13:02:04 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,160 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import { Form, Field } from "react-final-form";
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const App = () => {
+  const onSubmit = async values => {
+    await sleep(300);
+    window.alert(JSON.stringify(values, 0, 2));
+  };
+
+  return (
+    <div className="App">
+      <h1> This is final form</h1>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{ stooge: "larry", employed: false }}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>First Name</label>
+              <Field
+                name="firstName"
+                component="input"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <Field
+                name="lastName"
+                component="input"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+            <div>
+              <label>Employed</label>
+              <Field name="employed" component="input" type="checkbox" />
+            </div>
+            <div>
+              <label>Favorite Color</label>
+              <Field name="favoriteColor" component="select">
+                <option />
+                <option value="#ff0000">Red</option>
+                <option value="#00ff00">Green</option>
+                <option value="#0000ff">Blue</option>
+              </Field>
+            </div>
+            <div>
+              <label>Toppings</label>
+              <Field name="toppings" component="select" multiple>
+                <option value="chicken">Chicken</option>
+                <option value="ham">Ham</option>
+                <option value="mushrooms">Mushrooms</option>
+                <option value="cheese">Cheese</option>
+                <option value="tuna">Tuna</option>
+                <option value="pineapple">Pineapple</option>
+              </Field>
+            </div>
+            <div>
+              <label>Sauces</label>
+              <div>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="ketchup"
+                  />{" "}
+                  Ketchup
+                </label>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="mustard"
+                  />{" "}
+                  Mustard
+                </label>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="mayonnaise"
+                  />{" "}
+                  Mayonnaise
+                </label>
+                <label>
+                  <Field
+                    name="sauces"
+                    component="input"
+                    type="checkbox"
+                    value="guacamole"
+                  />{" "}
+                  Guacamole
+                </label>
+              </div>
+            </div>
+            <div>
+              <label>Best Stooge</label>
+              <div>
+                <label>
+                  <Field
+                    name="stooge"
+                    component="input"
+                    type="radio"
+                    value="larry"
+                  />{" "}
+                  Larry
+                </label>
+                <label>
+                  <Field
+                    name="stooge"
+                    component="input"
+                    type="radio"
+                    value="moe"
+                  />{" "}
+                  Moe
+                </label>
+                <label>
+                  <Field
+                    name="stooge"
+                    component="input"
+                    type="radio"
+                    value="curly"
+                  />{" "}
+                  Curly
+                </label>
+              </div>
+            </div>
+            <div>
+              <label>Notes</label>
+              <Field name="notes" component="textarea" placeholder="Notes" />
+            </div>
+            <div className="buttons">
+              <button type="submit" disabled={submitting || pristine}>
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={form.reset}
+                disabled={submitting || pristine}
+              >
+                Reset
+              </button>
+            </div>
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+          </form>
+        )}
+      />
+    </div>
+  );
+};
 
 export default App;
